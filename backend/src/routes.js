fd6ddb40310b77029cb64127e468eca3aef4af72
rabbit11 <pedro.coelho@dcomp.sor.ufscar.br>
2019-10-31 21:45:31 -0300

    backend is finally working!!!

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,8 @@
 const express = require('express')
 const apiID = require('steamapi')
 const api = require('steam-js-api')
-const bodyParser = require('body-parser')
 
 const routes = express.Router()
-
-routes.use(bodyParser.json())
 
 const steam = new apiID('A897E04835D9E1F7B63F373919191882')
 
@@ -16,60 +13,32 @@ const appID = 730 // We only want to check for one game
 const moreInfo = true // Provide more info (name, links)
 
 // With a callback
-const findUser = (steamID) => {
-    return new Promise((resolve, reject) => {
-        api.getStats(steamID, 730).then(result => {
-            console.log(result.data)
-            console.log(result.data.stats.gun.awp)
-            // apiResult = result
-        
-            return result
-        }).catch(console.error)
-    })
+const findUser = async (steamID) => {
+
+    const user = await api.getStats(steamID, 730)
+
+    return user
 }
 
-const findUserId = (username) => {
-    return new Promise((resolve, reject) => {
-        const link = 'https://steamcommunity.com/id/'
-        // const userLink = link.concat(username)
-        const userLink = link + username
-    
-        let id = steam.resolve(userLink).then(id => {
-            console.log('O id de ' + username + ' e: ' + id); // 76561198146931523
-    
-            return id
-        }).catch(console.error)
-    })
+const findUserId = async (username) => {
+    const link = 'https://steamcommunity.com/id/'
+    const userLink = link + username
+
+    const id = await steam.resolve(userLink)
+
+    return id
 }
 
-routes.get('/teste', (req,res) => {
+routes.get('/teste', (req, res) => {
     console.log('deu bom!!!!')
 
 })
 
-const getUserProfile = (username) => {
-    const user = findUserId(username).then(id => {
-        return findUser(id)
-    })
+routes.get("/profile/:name", async (req, res) => {
+    const userId = await findUserId(req.params.name)
+    const user = await findUser(userId)
 
-}
-
-routes.get("/profile/:name", (req, res) => {
-    // const userId = findUserId(req.params.name)
-
-    // const userProfile = findUser(userId)
-
-    // if (userId != '76561198146931523') {
-    //     console.log(userId)
-    // }
-    
-    // console.log(userProfile)
-
-    const user = getUserProfile(req.params.name)
-
-    console.log(user)
-    
-    res.json(user)
+    return res.json({ user: user })
 })
 
 module.exports = routes;
